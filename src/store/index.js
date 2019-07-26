b1f10797195a95e784e23da446d6ce7a6f1ca65d
Sun668 <947692259@qq.com>
2019-07-26 17:46:41 +0800

    Init: create frontend environment

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vuex from 'vuex'
+
+import Vue from 'vue'
+
+import actions from './actions'
+
+import mutations from './mutations'
+
+import getters from './getters'
+
+Vue.use(Vuex)
+
+const state = {
+
+}
+
+export default new Vuex.Store({
+
+  state,
+
+  getters,
+
+  actions,
+
+  mutations
+})
