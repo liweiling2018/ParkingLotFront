cfd93a2c5e753800e6cbfe031946ff742442fd08
Sun668 <947692259@qq.com>
2019-07-28 11:08:08 +0800

    Refactor: change form title

--- src/components/ParkingBoy/AddParkingBoyForm.vue
+++ src/components/ParkingBoy/AddParkingBoyForm.vue
@@ -1,8 +1,14 @@
 <template>
 
-<Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+<Form ref="formValidate" :title='title' :model="formValidate" :rules="ruleValidate" :label-width="80">
     <FormItem label="Name" prop="name">
         <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+    </FormItem>
+    <FormItem label="Age" prop="age">
+        <Input v-model="formValidate.age" placeholder="Enter your age" number></Input>
+    </FormItem>
+    <FormItem label="Phone" prop="phone">
+        <Input v-model="formValidate.phone" placeholder="Enter your age" number></Input>
     </FormItem>
     <FormItem label="Gender" prop="gender">
         <RadioGroup v-model="formValidate.gender">
@@ -20,6 +26,8 @@ export default {
             formValidate: {
                 name: '',
                 gender: '',
+                age: '',
+                phone: ''
             },
             ruleValidate: {
                 name: [
@@ -27,6 +35,12 @@ export default {
                 ],
                 gender: [
                     { required: true, message: 'Please select gender', trigger: 'change' }
+                ],
+                age: [
+                    { required: true, message: 'Please select age', trigger: 'change' }
+                ],
+                phone: [
+                    { required: true, message: 'Please select phone number', trigger: 'change' }
                 ]
             }
         }
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,35 +1,35 @@
 export default {
-    getConfig (state) {
-        if (process.env.NODE_ENV === 'development') {
-            return state.config.dev
-        } else if (process.env.type == 'stage') {
-            return state.config.stage
-        } else {
-            return state.config.prod
-        }
-    },
-    getParkingBoyList (state) {
-        if (process.env.NODE_ENV === 'development') {
-            return [{
-              id: '0',
-              name: '张三',
-              phone: '13416366089',
-              state: 0,
-              parkingLotList: [
-                { name: 'lot1'}
-              ]
-            },
-            {
-                id: '1',
-                name: '张四',
-                phone: '13416366089',
-                state: 0,
-                parkingLotList: [
-                  { name: 'lot1'}
-                ]
-              }
-            ]
-          }
-        return state.parkingBoyList
+  getConfig (state) {
+    if (process.env.NODE_ENV === 'development') {
+      return state.config.dev
+    } else if (process.env.type == 'stage') {
+      return state.config.stage
+    } else {
+      return state.config.prod
     }
+  },
+  getParkingBoyList (state) {
+    if (process.env.NODE_ENV === 'development') {
+      return [{
+        id: '0',
+        name: '张三',
+        phone: '13416366089',
+        state: 0,
+        parkingLotList: [
+          { name: 'lot1'}
+        ]
+      },
+      {
+        id: '1',
+        name: '张四',
+        phone: '13416366089',
+        state: 0,
+        parkingLotList: [
+          { name: 'lot1'}
+        ]
+        }
+      ]
+    }
+    return state.parkingBoyList
+  }
 }
