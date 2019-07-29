5c78b8e77c24531dc1f5598fceec0b822588336e
Sun668 <947692259@qq.com>
2019-07-29 18:09:13 +0800

    Refactor: add head in reserve park ui

--- src/views/AppointParking.vue
+++ src/views/AppointParking.vue
@@ -1,12 +1,24 @@
 <template>
 <div class="reserve-container">
-    <div class='reserve-div'>
-        <AppointPark></AppointPark>
-    </div>
-    <Divider type="vertical" />
-    <div class='reserve-div'>
-        <AppointFetch></AppointFetch>
-    </div>
+    <Layout>
+        <Header :style="{position: 'fixed', width: '100%'}">
+            <div class="reserve-logo"><h2>滴滴停车预约系统</h2> </div>
+        </Header>
+        <Content :style="{margin: '88px 20px 0',  minHeight: '500px'}">
+            <div class="reserve-container">
+                <div class='reserve-div'>
+                <AppointPark></AppointPark>
+                </div>
+                <Divider type="vertical" />
+                <div class='reserve-div'>
+                    <AppointFetch></AppointFetch>
+                </div>
+            </div>
+            
+        </Content>
+        <Footer class="layout-footer-center">2018-2019 &copy; FocusTeam</Footer>
+    </Layout>
+    
 </div>
     
 
