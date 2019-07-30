317308face87cd76085f4da65535b967a2d67b2e
Sun668 <947692259@qq.com>
2019-07-31 00:51:58 +0800

    Debug: fix the falsh when init user message in grab order ui

--- src/components/GrabOrder/GrabOrderForm.vue
+++ src/components/GrabOrder/GrabOrderForm.vue
@@ -11,10 +11,11 @@
 import GrabOrderLoginForm from './GrabOrderLoginForm'
 import GrabOrderChangePassForm from './GrabOrderChangePassForm'
 import ParkingBoyMessage from './ParkingBoyMessage'
+import { parkingBoyLogin } from '../../assets/api/grabOrder'
 export default {
   data () {
     return {
-      userState: 0
+      userState: -1
     }
   },
   components: {
@@ -35,9 +36,28 @@ export default {
     },
     parkingBoyExit () {
       this.userState = 0
+    },
+    login (vm, user) {
+      parkingBoyLogin(vm, user, function (data) {
+        if (data.status == 200 ) {
+          vm.$store.commit('setUser', data.data)
+          localStorage.setItem('username', user.username)
+          localStorage.setItem('password', user.password)
+          vm.$emit('parkingBoyLoginSuccess')
+          vm.userState = 2
+        }
+        else {
+          vm.userState = 0
+        }
+      }, function (fail) {}, function (err) {
+
+      })
     }
   },
   mounted () {
+    let vm = this
+    let user = {username: localStorage.getItem('username'), password: localStorage.getItem('password')}
+    this.login(vm, user)
   }
 };
 </script>
